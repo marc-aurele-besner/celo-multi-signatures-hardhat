f93254d1db49ba45c3e2cd39fc24c4ad899ea316
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-03-04 15:36:55 -0500

    add hardhat settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,53 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-awesome-cli");
 require("dotenv").config();
 
+const {
+  CELO_MAINNET_MNEMONIC,
+  CELO_TESTNET_MNEMONIC,
+  CELO_API_KEY
+} = process.env
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  networks: {
+    localhost: {
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk',
+      },
+      chainId: 31337,
+    },
+    celoMainnet: {
+      url: `https://forno.celo.org`,
+      accounts: {
+        mnemonic: [`${CELO_MAINNET_MNEMONIC}`],
+      },
+      chainId: 44787,
+    },
+    celoAlfajores: {
+      url: `https://alfajores-forno.celo-testnet.org`,
+      accounts: {
+        mnemonic: [`${CELO_TESTNET_MNEMONIC}`],
+      },
+      chainId: 44787,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      celoMainnet: `${CELO_API_KEY}`,
+      celoAlfajores: `${CELO_API_KEY}`,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
